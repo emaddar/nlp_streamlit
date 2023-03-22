f34c28c5f66f1e49f09d7ae678a10d7959e62a18
Emad_Simplon <e.a.darwich@gmail.com>
2023-03-22 12:33:21 +0100

    add cam

--- nlp_sreamlit.py
+++ nlp_sreamlit.py
@@ -236,6 +236,13 @@ if button:
             with st.expander("See text displacy"):
                 dep_svg = displacy.render(doc_our_model, style="ent", jupyter=False)
                 st.markdown(dep_svg, unsafe_allow_html=True)
+    st.header("test")
+    from transformers import AutoTokenizer, AutoModelForTokenClassification
+    tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/camembert-ner")
+    model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner")
+    from transformers import pipeline
 
+    nlp = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+    st.write(nlp("Apple est créée le 1er avril 1976 dans le garage de la maison d'enfance de Steve Jobs à Los Altos en Californie par Steve Jobs, Steve Wozniak et Ronald Wayne14, puis constituée sous forme de société le 3 janvier 1977 à l'origine sous le nom d'Apple Computer, mais pour ses 30 ans et pour refléter la diversification de ses produits, le mot « computer » est retiré le 9 janvier 2015."))
 
    
